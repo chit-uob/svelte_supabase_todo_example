a0fbf4ca8c5f009a9687ecfdea2fb8fb553662cc
chit-uob <sample@email.com>
2023-01-05 16:48:17 +0000

    fixed files in wrong directory

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/svelte_supabase_todo_example/',
   plugins: [svelte()],
   resolve: {
     alias: {
